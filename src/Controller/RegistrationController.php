1b52e5969f203a7f772ea1bb99d138492940bf3e
Kocmonavtik <chapligin141299@gmail.com>
2021-07-07 21:49:39 +0300

    Дополнение

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\InfoCustomerFormType;
 use App\Form\RegistrationFormType;
 use App\Security\LoginFormAuthentificatorAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,9 +13,46 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+use RetailCrm\Api\Interfaces\ClientExceptionInterface;
+use RetailCrm\Api\Interfaces\ApiExceptionInterface;
+use RetailCrm\Api\Factory\SimpleClientFactory;
+use RetailCrm\Api\Model\Entity\Customers\Customer;
+use RetailCrm\Api\Model\Request\Customers\CustomersCreateRequest;
+use Symfony\Component\Validator\Constraints\Date;
+
 
 class RegistrationController extends AbstractController
 {
+    /**
+     * @Route("/registerTest", name="app_registerTest")
+     */
+    public function registerTest(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthentificatorAuthenticator $authenticator): Response
+    {
+        $customer=new Customer();
+        $form = $this->createForm(InfoCustomerFormType::class,$customer);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $client = SimpleClientFactory::createClient('https://popova.retailcrm.ru', 'eVsrX4drzsw35chftqiSbTbGgbLtaPbN');
+            $requestUser = new CustomersCreateRequest();
+            $requestUser->customer = $customer;
+
+            try {
+                $response = $client->customers->create($requestUser);
+            } catch (ApiExceptionInterface | ClientExceptionInterface $exception) {
+                echo $exception; // Every ApiExceptionInterface instance should implement __toString() method.
+                exit(-1);
+            }
+            echo 'Customer ID: ' . $response->id;
+            return new RedirectResponse($this->generateUrl('about'));
+        }
+
+
+        return $this->render('registration/test.html.twig', [
+            'registrationTestForm' => $form->createView(),
+        ]);
+    }
+
+
     /**
      * @Route("/register", name="app_register")
      */
@@ -25,7 +63,7 @@ class RegistrationController extends AbstractController
             return new RedirectResponse($this->generateUrl('about'));
         }
         $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form = $this->createForm(RegistrationFormType::class,null);//$user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -36,11 +74,53 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
-
+            $user->setEmail($form->get('email')->getData());
+            if($this->getDoctrine()->getRepository(User::class)->findOneBy(array('email'=>$user->getEmail()))!=null){
+                return $this->render('registration/register.html.twig', [
+                    'registrationForm' => $form->createView(),
+                    'error'=>'Пользователь с введенным email уже существует',
+                ]);
+            }
+            //$user->setEmail($form->get('email')->getData());
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
+
+            //$this->setInfoToCRM($user->getEmail(),$form);
+
+
+
+            $objectUser=$this->getDoctrine()->getRepository(User::class)->findOneBy(array('email'=>$user->getEmail()));
+            $id=$objectUser->getId();
+
+            $client = SimpleClientFactory::createClient('https://popova.retailcrm.ru', 'eVsrX4drzsw35chftqiSbTbGgbLtaPbN');
+            $requestUser = new CustomersCreateRequest();
+            $requestUser->customer = new Customer();
+
+            //$request->site = 'aliexpress';
+            $requestUser->customer->externalId=$id;
+            //$requestUser->customer->sex=$form->get('gender')->getData();
+            $requestUser->customer->birthday=$form->get('birthday')->getData();
+            //$requestUser->customer->address->text='Липецк';//$form->get('address')->getData();
+            //$requestUser->customer->address[]->text=$form->get('address')->getData();
+            //$requestUser->customer->phones[]->number=$form->get('phoneNumber')->getData();
+            //$requestUser->customer->phones[]->number=array($form->get('phoneNumber')->getData());
+            //$requestUser->customer->phones[0]->number=array($form->get('phoneNumber')->getData());
+            //$requestUser->customer->phones[0]->['number']=$form->get('phoneNumber')->getData();
+            //$requestUser->customer->phones-
+            $requestUser->customer->email = $form->get('email')->getData();
+            $requestUser->customer->firstName = $form->get('name')->getData();
+            $requestUser->customer->lastName = $form->get('surname')->getData();
+            $requestUser->customer->patronymic=$form->get('patronymic')->getData();
+            try {
+                $response = $client->customers->create($requestUser);
+            } catch (ApiExceptionInterface | ClientExceptionInterface $exception) {
+                echo $exception; // Every ApiExceptionInterface instance should implement __toString() method.
+                exit(-1);
+            }
+            echo 'Customer ID: ' . $response->id;
+
 
             return $guardHandler->authenticateUserAndHandleSuccess(
                 $user,
@@ -52,6 +132,7 @@ class RegistrationController extends AbstractController
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
+            'error'=>'',
         ]);
     }
 }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,7 +3,10 @@
 namespace App\Form;
 
 use App\Entity\User;
+use phpDocumentor\Reflection\Type;
+use RetailCrm\Api\Model\Entity\Customers\Customer;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -11,13 +14,90 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
+            ->add('surname',null,[
+                'required'=>true,
+                'constraints'=>[
+                    new Length([
+                        'maxMessage'=>'Максимальное число символов 255',
+                        'max'=>255,
+                    ]),
+                ],
+                'label'=>'Фамилия',
+                'attr'=>[
+                    'class'=>'validate',
+                ],
+            ])
+            ->add('name',null,[
+                'required'=>true,
+                'constraints'=>[
+                    new Length([
+                        'maxMessage'=>'Максимальное число символов 255',
+                        'max'=>255,
+                    ]),
+                ],
+                'label'=>'Имя',
+                'attr'=>[
+                    'class'=>'validate',
+                ],
+            ])
+            ->add('patronymic',null,[
+                'required'=>false,
+                'constraints'=>[
+                    new Length([
+                        'maxMessage'=>'Максимальное число символов 255',
+                        'max'=>255,
+                    ]),
+                ],
+                'label'=>'Отчество',
+                'attr'=>[
+                    'class'=>'validate',
+                ],
+            ])
+            ->add('phoneNumber',null,[
+                'required'=>true,
+                'constraints'=>[
+                    new Length([
+                        'maxMessage'=>'Максимальное число символов 20',
+                        'max'=>20,
+                    ]),
+                ],
+                'label'=>'Номер телефона',
+                'attr'=>[
+                    'class'=>'validate',
+                ],
+            ])
+            ->add('gender', ChoiceType::class,[
+                'label'=>'Выберите пол',
+                'choices'=>[
+                    'Мужской'=>'Мужской',
+                    'Женский'=>'Женский',
+                ],
+            ])
+            ->add('address',null,[
+                'required'=>true,
+                'constraints'=>[
+                    new Length([
+                        'maxMessage'=>'Максимальное число символов 512',
+                        'max'=>512,
+                    ]),
+                ],
+                'label'=>'Адрес доставки',
+                'attr'=>[
+                    'class'=>'validate',
+                ],
+            ])
+            ->add('email',null,[
+                'required'=>true,
+            ])
+
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -33,23 +113,30 @@ class RegistrationFormType extends AbstractType
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Пожалуйста, введите пароль',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Ваш пароль должен быть не менее {{ limit }} символов',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
             ])
+            ->add('birthday', BirthdayType::class, [
+                'label' => 'День рождения',
+                'required' => true,
+                'widget'=>'single_text',
+                'format'=>'yyyy-MM-dd'
+            ])
+
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults([
+        /*$resolver->setDefaults([
             'data_class' => User::class,
-        ]);
+        ]);*/
     }
 }
