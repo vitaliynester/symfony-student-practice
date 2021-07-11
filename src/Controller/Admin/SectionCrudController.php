8eec97e080dc0c0e0808cc35f50079ce240ff632
Vitaly Posadnev <elemabor@gmail.com>
2021-07-11 20:18:22 +0300

    add: base admin section crud controller

---
+++ src/Controller/Admin/SectionCrudController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Section;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class SectionCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Section::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Разделы')
+            ->setEntityLabelInSingular('Раздел');
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        yield IdField::new('id')->hideOnForm();
+        yield AssociationField::new('parent')->setLabel('Родитель');
+        yield TextField::new('name')->setLabel('Название')->setRequired(true);
+        yield IdField::new('xmlId')->setLabel('Идентификатор XML');
+    }
+}
