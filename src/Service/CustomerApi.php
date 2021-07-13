33ca4f4e9e6edc8def434920345100a9dbdb8955
Vitaly Posadnev <elemabor@gmail.com>
2021-07-13 17:55:24 +0300

    update: PSR CustomerApi.php

--- src/Service/CustomerApi.php
+++ src/Service/CustomerApi.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Service;
 
 use RetailCrm\Api\Factory\SimpleClientFactory;
@@ -8,32 +9,31 @@ use RetailCrm\Api\Model\Entity\Customers\Customer;
 use RetailCrm\Api\Model\Entity\Customers\CustomerAddress;
 use RetailCrm\Api\Model\Entity\Customers\CustomerPhone;
 use RetailCrm\Api\Model\Request\Customers\CustomersCreateRequest;
-use phpDocumentor\Reflection\Types\This;
-use RetailCrm\Api\Client;
 
 class CustomerApi
 {
-    public function createCustomer($id,$form,$url, $api)
+    public function createCustomer($id, $form, $url, $api)
     {
         $client = SimpleClientFactory::createClient($url, $api);
         $requestUser = new CustomersCreateRequest();
         $requestUser->customer = new Customer();
 
-        $requestUser->customer->externalId=$id;
-        $requestUser->customer->sex=$form->get('gender')->getData();
-        $requestUser->customer->address= new CustomerAddress();
-        $requestUser->customer->address->text=$form->get('address')->getData();
-        $requestUser->customer->phones= [new CustomerPhone($form->get('phoneNumber')->getData())];
+        $requestUser->customer->externalId = $id;
+        $requestUser->customer->sex = $form->get('gender')->getData();
+        $requestUser->customer->address = new CustomerAddress();
+        $requestUser->customer->address->text = $form->get('address')->getData();
+        $requestUser->customer->phones = [new CustomerPhone($form->get('phoneNumber')->getData())];
         $requestUser->customer->email = $form->get('email')->getData();
         $requestUser->customer->firstName = $form->get('name')->getData();
         $requestUser->customer->lastName = $form->get('surname')->getData();
-        $requestUser->customer->patronymic=$form->get('patronymic')->getData();
+        $requestUser->customer->patronymic = $form->get('patronymic')->getData();
 
         try {
             $client->customers->create($requestUser);
+
             return true;
-        }catch (ApiExceptionInterface | ClientExceptionInterface $exception) {
-            return $exception; // Every ApiExceptionInterface instance should implement __toString() method.
+        } catch (ApiExceptionInterface | ClientExceptionInterface $exception) {
+            return $exception;
         }
     }
 }
