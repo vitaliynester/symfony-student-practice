2a69ecf3bea0063e5a38f0bc6d5c222da828ed9e
Vitaly Posadnev <elemabor@gmail.com>
2021-07-13 17:56:06 +0300

    update: PSR OrderApi.php

--- src/Service/OrderApi.php
+++ src/Service/OrderApi.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Service;
 
 use App\Entity\User;
@@ -6,7 +7,6 @@ use DateInterval;
 use DateTime;
 use RetailCrm\Api\Enum\ByIdentifier;
 use RetailCrm\Api\Enum\CountryCodeIso3166;
-use RetailCrm\Api\Enum\Customers\CustomerType;
 use RetailCrm\Api\Factory\SimpleClientFactory;
 use RetailCrm\Api\Interfaces\ApiExceptionInterface;
 use RetailCrm\Api\Interfaces\ClientExceptionInterface;
@@ -15,17 +15,11 @@ use RetailCrm\Api\Model\Entity\Orders\Delivery\OrderDeliveryAddress;
 use RetailCrm\Api\Model\Entity\Orders\Delivery\SerializedOrderDelivery;
 use RetailCrm\Api\Model\Entity\Orders\Items\Offer;
 use RetailCrm\Api\Model\Entity\Orders\Items\OrderProduct;
-use RetailCrm\Api\Model\Entity\Orders\Items\PriceType;
 use RetailCrm\Api\Model\Entity\Orders\Items\Unit;
 use RetailCrm\Api\Model\Entity\Orders\Order;
 use RetailCrm\Api\Model\Entity\Orders\Payment;
-use RetailCrm\Api\Model\Entity\Orders\SerializedRelationCustomer;
-use RetailCrm\Api\Model\Entity\References\DeliveryService;
 use RetailCrm\Api\Model\Request\BySiteRequest;
 use RetailCrm\Api\Model\Request\Orders\OrdersCreateRequest;
-use RetailCrm\Api\Model\Response\Orders\OrdersGetResponse;
-use RetailCrm\Api\ResourceGroup\References;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class OrderApi
 {
@@ -38,14 +32,14 @@ class OrderApi
 
     public function createOrder(User $customer, $form)
     {
-        $request         = new OrdersCreateRequest();
-        $order           = new Order();
-        $payment         = new Payment();
-        $delivery        = new SerializedOrderDelivery();
+        $request = new OrdersCreateRequest();
+        $order = new Order();
+        $payment = new Payment();
+        $delivery = new SerializedOrderDelivery();
         $deliveryAddress = new OrderDeliveryAddress();
-        $items            = [];
+        $items = [];
 
-        $payment->type   = $form->get('payment_type')->getData();
+        $payment->type = $form->get('payment_type')->getData();
         $payment->status = 'paid';
         $payment->amount = $this->getPaymentAmount($customer);
         $payment->paidAt = new DateTime();
@@ -54,7 +48,7 @@ class OrderApi
 
         $delivery->code = $form->get('delivery_type')->getData();
         $delivery->address = $deliveryAddress;
-        $delivery->cost    = 0;
+        $delivery->cost = 0;
         $delivery->netCost = 0;
 
         foreach ($customer->getCartItems() as $cartItem) {
@@ -62,8 +56,8 @@ class OrderApi
             $crmOffer = new Offer();
 
             $crmOffer->name = $offer->getName();
-            $crmOffer->article =  $offer->getName();
-            $crmOffer->xmlId =  $offer->getXmlId();
+            $crmOffer->article = $offer->getName();
+            $crmOffer->xmlId = $offer->getXmlId();
             $crmOffer->unit = new Unit($offer->getQuantity(), $offer->getUnit(), $offer->getUnit());
 
             $item = new OrderProduct();
@@ -75,35 +69,26 @@ class OrderApi
             $items[] = $item;
         }
 
-        $order->delivery      = $delivery;
-        $order->items         = $items;
-        $order->payments      = [$payment];
-        $order->orderMethod   = 'phone';
-        $order->countryIso    = CountryCodeIso3166::RUSSIAN_FEDERATION;
-        $order->firstName     = $form->get('name')->getData();
-        $order->lastName      = $form->get('surname')->getData();
-        $order->patronymic    = $form->get('patronymic')->getData();
-        $order->phone         = $form->get('phone')->getData();
-        $order->email         = $form->get('email')->getData();
-        $order->customer      = new Customer();
+        $order->delivery = $delivery;
+        $order->items = $items;
+        $order->payments = [$payment];
+        $order->orderMethod = 'phone';
+        $order->countryIso = CountryCodeIso3166::RUSSIAN_FEDERATION;
+        $order->firstName = $form->get('name')->getData();
+        $order->lastName = $form->get('surname')->getData();
+        $order->patronymic = $form->get('patronymic')->getData();
+        $order->phone = $form->get('phone')->getData();
+        $order->email = $form->get('email')->getData();
+        $order->customer = new Customer();
         $order->customer->externalId = $customer->getId();
-        $order->status        = 'assembling';
-        $order->shipmentDate  = (new DateTime())->add(new DateInterval('P7D'));
-        $order->shipped       = false;
+        $order->status = 'assembling';
+        $order->shipmentDate = (new DateTime())->add(new DateInterval('P7D'));
+        $order->shipped = false;
 
         $request->order = $order;
 
         try {
             $response = $this->client->orders->create($request);
-            return $response;
-        } catch (ApiExceptionInterface | ClientExceptionInterface $exception) {
-            return $exception;
-        }
-    }
-
-    public function getOrderById($orderId) {
-        try {
-            $response = $this->client->orders->get($orderId, new BySiteRequest(ByIdentifier::ID));
             return $response;
         } catch (ApiExceptionInterface | ClientExceptionInterface $exception) {
             return $exception;
@@ -120,5 +105,14 @@ class OrderApi
         }
 
         return $amount;
+    }
+
+    public function getOrderById($orderId)
+    {
+        try {
+            return $this->client->orders->get($orderId, new BySiteRequest(ByIdentifier::ID));
+        } catch (ApiExceptionInterface | ClientExceptionInterface $exception) {
+            return $exception;
+        }
     }
 }
