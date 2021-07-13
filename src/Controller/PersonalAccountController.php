7437e1fd662b6c0a8d1dbae7c20869807f90be0e
Vitaly Posadnev <elemabor@gmail.com>
2021-07-13 18:24:52 +0300

    update: PSR CustomerApi.php and PersonalAccountController.php

--- src/Controller/PersonalAccountController.php
+++ src/Controller/PersonalAccountController.php
@@ -3,62 +3,57 @@
 namespace App\Controller;
 
 use App\Form\PersonalAccountType;
+use App\Service\CustomerApi;
 use Knp\Component\Pager\PaginatorInterface;
-use RetailCrm\Api\Enum\ByIdentifier;
-use RetailCrm\Api\Factory\SimpleClientFactory;
-use RetailCrm\Api\Interfaces\ApiExceptionInterface;
-use RetailCrm\Api\Interfaces\ClientExceptionInterface;
 use RetailCrm\Api\Model\Entity\Customers\Customer;
 use RetailCrm\Api\Model\Entity\Customers\CustomerAddress;
 use RetailCrm\Api\Model\Entity\Customers\CustomerPhone;
-use RetailCrm\Api\Model\Filter\Orders\OrderFilter;
 use RetailCrm\Api\Model\Request\Customers\CustomersEditRequest;
-use RetailCrm\Api\Model\Request\Orders\OrdersRequest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use App\Service\CustomerApi;
 
 class PersonalAccountController extends AbstractController
 {
     /**
-     * @Route("/personalAccount", name="personal_account")
+     * @Route("/personal_account", name="personal_account")
      */
     public function index(): Response
     {
         $user = $this->getUser();
-        $Api =new CustomerApi();
-        $customer = $Api->checkCustomer( $user, $this->getParameter('url'), $this->getParameter('apiKey') );
+        $Api = new CustomerApi();
+        $customer = $Api->checkCustomer($user, $this->getParameter('url'), $this->getParameter('apiKey'));
+
         return $this->render('personal_account/index.html.twig', [
             'user' => $customer,
         ]);
     }
 
     /**
-     * @Route("/personalAccount/edit", name="personal_account_edit")
+     * @Route("/personal_account/edit", name="personal_account_edit")
      */
     public function edit(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $user = $this->getUser();
-        $form = $this->createForm(PersonalAccountType::class,null);
+        $form = $this->createForm(PersonalAccountType::class, null);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $requestUser = new CustomersEditRequest();
             $requestUser->customer = new Customer();
             $requestUser->site = 'b12-skillum-ru';
-            if (!is_null($form->get('phoneNumber')->getData())){
+            if (null !== $form->get('phoneNumber')->getData()) {
                 $requestUser->customer->phones = [new CustomerPhone($form->get('phoneNumber')->getData())];
             }
-            if (!is_null($form->get('gender')->getData())){
+            if (null !== $form->get('gender')->getData()) {
                 $requestUser->customer->sex = $form->get('gender')->getData();
             }
-            if (!is_null($form->get('address')->getData())){
+            if (null !== $form->get('address')->getData()) {
                 $requestUser->customer->address = new CustomerAddress();
                 $requestUser->customer->address->text = $form->get('address')->getData();
             }
-            if (!is_null($form->get('plainPassword')->getData())){
+            if (null !== $form->get('plainPassword')->getData()) {
                 $user->setPassword(
                     $passwordEncoder->encodePassword(
                         $user,
@@ -70,24 +65,27 @@ class PersonalAccountController extends AbstractController
                 $entityManager->flush();
             }
             $user = $this->getUser();
-            $Api =new CustomerApi();
-            $Api->changeCustomer( $user, $this->getParameter('url'), $this->getParameter('apiKey'), $requestUser);
+            $Api = new CustomerApi();
+            $Api->changeCustomer($user, $this->getParameter('url'), $this->getParameter('apiKey'), $requestUser);
+
             return $this->redirectToRoute('personal_account');
         }
+
         return $this->render('personal_account/edit.html.twig', [
             'form' => $form->createView(),
         ]);
     }
 
     /**
-     * @Route("/personalAccount/ordersHystory/{page}", name="personal_account_orders_history", methods={"GET","POST"})
+     * @Route("/personal_account/ordersHystory/{page}", name="personal_account_orders_history", methods={"GET","POST"})
      */
     public function ordersHistory(PaginatorInterface $paginator, $page): Response
     {
         $user = $this->getUser();
-        $Api =new CustomerApi();
-        $order = $Api->getHistoryOrders( $user, $this->getParameter('url'), $this->getParameter('apiKey') );
-        $pagination = $paginator->paginate($order,$page,1);
+        $Api = new CustomerApi();
+        $order = $Api->getHistoryOrders($user, $this->getParameter('url'), $this->getParameter('apiKey'));
+        $pagination = $paginator->paginate($order, $page, 1);
+
         return $this->render('personal_account/historyOrders.html.twig', [
             'pagination' => $pagination,
         ]);
