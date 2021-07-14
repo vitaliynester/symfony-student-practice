cce857dff6528933f8be6cab7fadc9081ef4d061
Vitaly Posadnev <elemabor@gmail.com>
2021-07-14 20:59:07 +0300

    add: label for plain password on RegistrationFormType.php

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,32 +2,31 @@
 
 namespace App\Form;
 
-//use MongoDB\BSON\Regex;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Validator\Constraints\Regex;
-use Symfony\Component\Validator\Constraints\Email;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('surname',null,[
-                'required'=>true,
-                'constraints'=>[
+            ->add('surname', null, [
+                'required' => true,
+                'constraints' => [
                     new Length([
-                        'min'=>3,
-                        'minMessage'=>'Минимальное число символов - {{ limit }}',
-                        'max'=>255,
+                        'min' => 3,
+                        'minMessage' => 'Минимальное число символов - {{ limit }}',
+                        'max' => 255,
                     ]),
                     new Regex([
                         'match' => true,
@@ -35,93 +34,88 @@ class RegistrationFormType extends AbstractType
                         'message' => 'Допустимо написание русскими буквами,пробелами и дефисами',
                     ]),
                 ],
-                'label'=>'Фамилия',
-                'attr'=>[
-                    'class'=>'validate',
+                'label' => 'Фамилия',
+                'attr' => [
+                    'class' => 'validate',
                 ],
             ])
-
-            ->add('name',null,[
-                'required'=>true,
-                'constraints'=>[
+            ->add('name', null, [
+                'required' => true,
+                'constraints' => [
                     new Length([
-                        'min'=>3,
-                        'minMessage'=>'Минимальное число символов - {{ limit }}',
-                        'max'=>255,
+                        'min' => 3,
+                        'minMessage' => 'Минимальное число символов - {{ limit }}',
+                        'max' => 255,
                     ]),
                     new Regex([
-                        'match'=>true,
-                        'pattern'=>'/^[а-яё -]+$/ui',
-                        'message'=>'Допустимо написание русскими буквами,пробелами и дефисами'
+                        'match' => true,
+                        'pattern' => '/^[а-яё -]+$/ui',
+                        'message' => 'Допустимо написание русскими буквами,пробелами и дефисами',
                     ]),
                 ],
-                'label'=>'Имя',
-                'attr'=>[
-                    'class'=>'validate',
+                'label' => 'Имя',
+                'attr' => [
+                    'class' => 'validate',
                 ],
             ])
-
-            ->add('patronymic',null,[
-                'required'=>false,
-                'constraints'=>[
+            ->add('patronymic', null, [
+                'required' => false,
+                'constraints' => [
                     new Length([
-                        'maxMessage'=>'Максимальное число символов - {{ limit }}',
-                        'max'=>255,
+                        'maxMessage' => 'Максимальное число символов - {{ limit }}',
+                        'max' => 255,
                     ]),
                     new Regex([
-                        'match'=>true,
-                        'pattern'=>'/^[а-яё -]+$/ui',
-                        'message'=>'Допустимо написание русскими буквами,пробелами и дефисами'
+                        'match' => true,
+                        'pattern' => '/^[а-яё -]+$/ui',
+                        'message' => 'Допустимо написание русскими буквами,пробелами и дефисами',
                     ]),
                 ],
-                'label'=>'Отчество',
-                'attr'=>[
-                    'class'=>'validate',
+                'label' => 'Отчество',
+                'attr' => [
+                    'class' => 'validate',
                 ],
             ])
-
-            ->add('phoneNumber',null,[
-                'required'=>true,
-                'constraints'=>[
+            ->add('phoneNumber', null, [
+                'required' => true,
+                'constraints' => [
                     new Length([
-                        'maxMessage'=>'Максимальное число символов {{ limit }}',
-                        'max'=>20,
+                        'maxMessage' => 'Максимальное число символов {{ limit }}',
+                        'max' => 20,
                     ]),
                     new Regex([
-                        'match'=>true,
-                        'pattern'=>'/^(\\+7|7|8)?[\\s\\-]?\\(?[489][0-9]{2}\\)?[\\s\\-]?[0-9]{3}[\\s\\-]?[0-9]{2}[\\s\\-]?[0-9]{2}$/',
-                        'message'=>'Недопустимое написание номера телефона'
+                        'match' => true,
+                        'pattern' => '/^(\\+7|7|8)?[\\s\\-]?\\(?[489][0-9]{2}\\)?[\\s\\-]?[0-9]{3}[\\s\\-]?[0-9]{2}[\\s\\-]?[0-9]{2}$/',
+                        'message' => 'Недопустимое написание номера телефона',
                     ]),
                 ],
-                'label'=>'Номер телефона',
-                'attr'=>[
-                    'class'=>'validate',
+                'label' => 'Номер телефона',
+                'attr' => [
+                    'class' => 'validate',
                 ],
             ])
-
-            ->add('gender', ChoiceType::class,[
-                'label'=>'Выберите пол',
-                'choices'=>[
-                    'Мужской'=>'male',
-                    'Женский'=>'female',
+            ->add('gender', ChoiceType::class, [
+                'label' => 'Выберите пол',
+                'choices' => [
+                    'Мужской' => 'male',
+                    'Женский' => 'female',
                 ],
             ])
-
-            ->add('address',null,[
-                'required'=>true,
-                'constraints'=>[
+            ->add('address', null, [
+                'required' => true,
+                'constraints' => [
                     new Length([
-                        'maxMessage'=>'Максимальное число символов {{ limit }}',
-                        'max'=>512,
+                        'maxMessage' => 'Максимальное число символов {{ limit }}',
+                        'max' => 512,
                     ]),
                 ],
-                'label'=>'Адрес доставки',
-                'attr'=>[
-                    'class'=>'validate',
+                'label' => 'Адрес доставки',
+                'attr' => [
+                    'class' => 'validate',
                 ],
             ])
-            ->add('email',EmailType::class,[
-                'required'=>true,
+            ->add('email', EmailType::class, [
+                'required' => true,
                 'constraints' => [
                     new Email([
                         'message' => 'Email введен не корректно!',
@@ -135,21 +129,20 @@ class RegistrationFormType extends AbstractType
                     'class' => 'validate',
                 ],
             ])
-
             ->add('agreeTerms', CheckboxType::class, [
-                'required'=>true,
+                'required' => true,
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
                         'message' => 'Необходимо ваше соглашение с использованием персональных данных',
                     ]),
                 ],
-                'label'=>'Согласен с использованием моих персональных данных'
+                'label' => 'Согласен с использованием моих персональных данных',
             ])
-
             ->add('plainPassword', PasswordType::class, [
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
+                'label' => 'Пароль',
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Пожалуйста, введите пароль',
@@ -160,14 +153,12 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-
         ]);
     }
 }
