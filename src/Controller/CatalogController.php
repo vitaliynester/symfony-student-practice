501f00a8e64792a49a7e924bae1038ae55081ca0
Сухоруков Кирилл Олегович <sukhorukov.kirill23@gmail.com>
2021-07-15 20:31:55 +0300

    fix: catalog

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -22,69 +22,62 @@ class CatalogController extends AbstractController
     /**
      * @Route("/catalog/{parId}/", name="catalog", methods={"GET"})
      */
-    public function index(SectionRepository $sectRep,PaginatorInterface $paginator,Request $request, $parId): Response 
+    public function index(SectionRepository $sectRep, PaginatorInterface $paginator, Request $request, $parId): Response
     {
         $pageRequest = $request->query->getInt('page', 1);
-        if ($pageRequest <= 0) 
-        {
+        if ($pageRequest <= 0) {
             $pageRequest = 1;
         }
         $qb = $sectRep->createQueryBuilder('s');
-        $sectData =  $qb->where('s.id = :parId')
-        ->setParameter('parId',$parId)
-        ->orWhere('s.parent = :parId')
-        ->setParameter('parId',$parId)
-        ->orderBy('s.id')
-        ->getQuery()
-        ->getResult();
+        $sectData = $qb->where('s.id = :parId')
+            ->setParameter('parId', $parId)
+            ->orWhere('s.parent = :parId')
+            ->setParameter('parId', $parId)
+            ->orderBy('s.id')
+            ->getQuery()
+            ->getResult();
         $offerData = [];
-        foreach($sectData as $section)
-        {
+        foreach ($sectData as $section) {
             $products = $section->getProducts();
-            foreach($products as $product)
-            {
+            foreach ($products as $product) {
                 $offers = $product->getOffers();
-                foreach($offers as $offer)
-                {
+                foreach ($offers as $offer) {
                     array_push($offerData, $offer);
                 }
             }
         }
         $mainSecion = $sectData[0];
         $childSections = [];
-        foreach($sectData as $section)
-        {
-            if($section->getId() != $parId)
-            {
+        foreach ($sectData as $section) {
+            if ($section->getId() != $parId) {
                 array_push($childSections, $section);
             }
         }
-        $pagination = $paginator->paginate($offerData,$pageRequest,9);
+        $pagination = $paginator->paginate($offerData, $pageRequest, 9);
         return $this->render('catalog/index.html.twig', ['parentSection' => $mainSecion,
-            'subSections' => $childSections, 'pagination' => $pagination,
+            'subSections' => $childSections, 'pagination' => $pagination, 'sections' => $sectData
         ]);
     }
 
     /**
      * @Route("/offer/{offerId}", name="offer", methods={"GET","POST"})
      */
-    public function offer( OfferRepository $offerRep, ProductRepository $prodRep, $offerId,Request $request): Response
+    public function offer(OfferRepository $offerRep, ProductRepository $prodRep, $offerId, Request $request): Response
     {
         $offerData = $offerRep->findOneBy(['id' => $offerId]);
         $form = $this->createForm(CartItemType::class, null);
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) 
-        {
-            $response = $this->forward('App\Controller\CartController::new', 
-            [
-                'quantity'  => $form->get('quantity')->getData(),
-                'offer' =>  $offerId,
-            ]);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $response = $this->forward('App\Controller\CartController::new',
+                [
+                    'quantity' => $form->get('quantity')->getData(),
+                    'offer' => $offerId,
+                ]);
             return $response;
         }
         $product = $offerData->getProduct();
         $offers = $product->getOffers();
-        return $this->render('catalog/offer.html.twig', [ 'mainOffer' => $offerData, 'similarOffers' => $offers, 'form' => $form->createView(),
+        return $this->render('catalog/offer.html.twig', ['mainOffer' => $offerData, 'similarOffers' => $offers, 'form' => $form->createView(),
         ]);
     }
 }
