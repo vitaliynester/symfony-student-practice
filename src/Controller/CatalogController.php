a856aa4bd73b157905a76378c56e86b5503d8fcb
G4zele <66208042+G4zele@users.noreply.github.com>
2021-07-16 15:57:33 +0300

    Catalog fix 4

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -92,8 +92,7 @@ class CatalogController extends AbstractController
         $offers = $product->getOffers();
         if ($form->isSubmitted() && $form->isValid() ) 
         {
-            var_dump($security->getUser()!= 0);
-            if(($form->get('quantity')->getData()<=0) and ($security->getUser()!= 0))
+            if(($form->get('quantity')->getData()<=0) or ($security->getUser()))
             {
                 return $this->render('catalog/offer.html.twig', ['mainOffer' => $offerData, 'similarOffers' => $offers,
                     'categories' => $items, 'form' => $form->createView(),
