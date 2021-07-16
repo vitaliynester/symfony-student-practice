b259a5a32c43b6a871d0422d96ec4ebeb2159a0f
G4zele <66208042+G4zele@users.noreply.github.com>
2021-07-16 16:31:01 +0300

    User null fix

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -94,6 +94,7 @@ class CatalogController extends AbstractController
         {
             if(($form->get('quantity')->getData()<=0) and ($security->getUser()!= NULL))
             {
+                echo('false');
                 return $this->render('catalog/offer.html.twig', ['mainOffer' => $offerData, 'similarOffers' => $offers,
                     'categories' => $items, 'form' => $form->createView(),
                     ]);
